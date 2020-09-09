dbaacfd61392aa1a999b48a7ceff9ae9ab2a3bc1
canberkb8 <canberkbibican@hotmail.com>
2020-09-09 17:04:27 +0300

    navbar fixed

--- lib/ui/home/view/home_view.dart
+++ lib/ui/home/view/home_view.dart
@@ -9,6 +9,7 @@ class HomeView extends BaseState {
   double get homePadding => MediaQuery.of(context).size.width * 0.05;
   double get homeButtonSize => MediaQuery.of(context).size.width * 0.08;
   double get navBarRadius => MediaQuery.of(context).size.width * 0.1;
+  double get navBarPadding => MediaQuery.of(context).size.width * 0.05;
   double get titleSize => MediaQuery.of(context).size.width * 0.1;
   double get subTitleSize => MediaQuery.of(context).size.width * 0.04;
   double get discountCardWidth => MediaQuery.of(context).size.width * 0.8;
@@ -25,55 +26,53 @@ class HomeView extends BaseState {
   Color get subTitleColor => Colors.grey[400];
 
   //Texts
-
   String get homeTitleText => "Choose the laundry service which you want";
   String get subTitleText => "Welcome to, The WashWorld";
+
+  //SizedBoxes
+  get zeroHeightSizedBox => SizedBox(height: 0);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: homeAppBar(),
-      body: Padding(
-          padding: EdgeInsets.all(homePadding),
-          child:
-              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-            Expanded(flex: 20, child: homeTitle()),
-            Expanded(flex: 5, child: homeSubTitle()),
-            Expanded(flex: 35, child: discountCard()),
-            Expanded(flex: 40, child: discountCard())
-          ])),
-      bottomNavigationBar: Padding(
-          padding: EdgeInsets.all(homePadding), child: navigationBar(context)),
+      body: homeBody(),
+      bottomNavigationBar: navigationBar(context),
     );
   }
 
   AppBar homeAppBar() {
     return AppBar(
-      backgroundColor: Colors.transparent,
-      bottomOpacity: 0,
-      elevation: 0,
-      leading: homeAppBarLeftIcon(context),
-      actions: <Widget>[homeAppBarRightIcon(context)],
-    );
+        backgroundColor: Colors.transparent,
+        bottomOpacity: 0,
+        elevation: 0,
+        leading: homeAppBarLeftIcon(context),
+        actions: [homeAppBarRightIcon(context)]);
   }
 
   IconButton homeAppBarLeftIcon(BuildContext context) {
     return IconButton(
-      icon: new Icon(
-        Icons.bubble_chart,
-        color: appBarIconColor,
-        size: homeButtonSize,
-      ),
-      onPressed: () => Navigator.of(context).pop(null),
-    );
+        icon: new Icon(Icons.bubble_chart,
+            color: appBarIconColor, size: homeButtonSize),
+        onPressed: () => Navigator.of(context).pop(null));
   }
 
   IconButton homeAppBarRightIcon(BuildContext context) {
     return IconButton(
-      icon:
-          new Icon(Icons.search, color: appBarIconColor, size: homeButtonSize),
-      onPressed: () => Navigator.of(context).pop(null),
-    );
+        icon: new Icon(Icons.search,
+            color: appBarIconColor, size: homeButtonSize),
+        onPressed: () => Navigator.of(context).pop(null));
+  }
+
+  Padding homeBody() {
+    return Padding(
+        padding: EdgeInsets.all(homePadding),
+        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          Expanded(flex: 20, child: homeTitle()),
+          Expanded(flex: 5, child: homeSubTitle()),
+          Expanded(flex: 35, child: discountCard()),
+          Expanded(flex: 40, child: discountCard())
+        ]));
   }
 
   Text homeTitle() {
@@ -88,23 +87,23 @@ class HomeView extends BaseState {
 
   Center discountCard() {
     return Center(
-      child: Card(
-        shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(discountCardRadius)),
-        margin: EdgeInsets.symmetric(
-            vertical: discountCardMargin, horizontal: discountCardMargin),
-        child: Padding(
-          padding: EdgeInsets.all(discountCardPadding),
-          child: Container(width: discountCardWidth),
-        ),
-      ),
-    );
+        child: Card(
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(discountCardRadius)),
+            margin: EdgeInsets.symmetric(
+                vertical: discountCardMargin, horizontal: discountCardMargin),
+            child: Padding(
+                padding: EdgeInsets.all(discountCardPadding),
+                child: Container(width: discountCardWidth))));
   }
 
   //Global widget olarak düzenlenicek
-  Theme navigationBar(BuildContext context) {
+  Padding navigationBar(BuildContext context) {
     int index = 0;
-    return Theme(
+    return Padding(
+      padding: EdgeInsets.only(
+          left: navBarPadding, right: navBarPadding, bottom: navBarPadding),
+      child: Theme(
         data: Theme.of(context).copyWith(canvasColor: navBarColor),
         child: ClipRRect(
           borderRadius: BorderRadius.circular(navBarRadius),
@@ -148,8 +147,8 @@ class HomeView extends BaseState {
                   title: zeroHeightSizedBox),
             ],
           ),
-        ));
+        ),
+      ),
+    );
   }
-
-  get zeroHeightSizedBox => SizedBox(height: 0);
 }
